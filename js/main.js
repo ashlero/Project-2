8b56d6816c5e24005090c27c8d8dc84240e48631
ashlero <alerouge1004@gmail.com>
2026-02-26 11:44:15 -0600

    added nav and toggle

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // JS scripts placed here
+const hamburger = document.querySelector('.hamburger-icon');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+  const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;
+  hamburger.setAttribute('aria-expanded',!expanded);
+  menu.classList.toggle('expanded');
+});
